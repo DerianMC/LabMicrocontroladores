b3f2a6573ce2bed32f0fd3b967a0755c16cc0376
DerianMC <derian.monge@ucr.ac.cr>
2023-03-27 09:57:26 -0600

    Funcionamiento correcto en la generación de números aleatorios y la presentación de estos en el display. Cada vez que se presiona el pulsador se genera un nuevo número pseudoaleatorio. Se utilizan números pseudoaleatorios debido a que no es posible generarlos con la cantidad de puertos I/O disponibles

--- Lab01/Lab01.c
+++ Lab01/Lab01.c
@@ -1,12 +1,17 @@
-#include <pic14/pic12f675.h>
- 
+
+#include <pic14/pic12f683.h>
+
+
 
 typedef unsigned int word ;
-word __at 0x2007 __CONFIG = (_BODEN_OFF ) ;
- 
-void delay (unsigned inttiempo);
-void dis (unsigned num);
+word __at 0x2007 __CONFIG = (_CP_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT & _FCMEN_OFF & _IESO_OFF & _MCLRE_OFF);
 
+
+void delay(unsigned int tiempo);	
+void dis(unsigned char num);
+unsigned char var1 = 0;
+unsigned char var2 = 0;
+unsigned char value = 0;
 
  
 char digit1[10] = {0b000000, 0b000001, 0b000010,
@@ -18,35 +23,67 @@ char digit2[10] = {0b100000, 0b100001, 0b100010,
 					0b100011, 0b100100, 0b100101,
 					0b100110, 0b100111, 0b110000,
 					0b110001}; 
+					
 
 
-void main(void)
-{
-	
-  	TRISIO = 0b00000000; //Poner todos los pines como salidas
-	TRISIO3 = 0b00000001;
-	GPIO = 0x00; //Poner pines en bajo
 
-	
 
-	
-	
- 
+
+void main() {
+    // Set GP1 and GP2 as outputs
+    TRISIO = 0b00001000; 
+    GPIO = 0b000000;   
   	unsigned int time = 1;
-	unsigned int num = 1;
- 
-    //Loop forever
-    while ( 1 )
-    {
-		num = 1;
-		dis(num);
+	unsigned char num = 1;
+	unsigned char seed = 0;
+	unsigned char var1 = 0;
+	unsigned char var2 = 0;
+	
+
+
+    while (1) {
+
+		num++;
+		unsigned char input_value = GP3;
+		
+		/*while (value == 0)
+		{
+		var1 = (num - (num / 100)*100)/10;
+		var2 = (num - (num / 100)*100-((num - (num / 100)*100)/10)*10);	
+		}
+		
+		GPIO = digit1[var1];
 		delay(time);
-		num = 2;
-		dis(num);
-		delay(time);	
+
+		GPIO = digit2[var2];
+		delay(time);	*/
+		if (input_value == 1) {
+			var1 = (num - (num / 100)*100)/10;
+			var2 = (num - (num / 100)*100-((num - (num / 100)*100)/10)*10);	
+			GPIO = digit1[0];
+			delay(time);
+			GPIO = digit2[0];
+			delay(time);
+		} 
+		
+		else {
+			GPIO = digit1[var1];
+			delay(time);
+			GPIO = digit2[var2];
+			delay(time);
+
+		}
+	
+
+
+
+
+
+
     }
- 
 }
+
+
 
 void delay(unsigned int tiempo)
 {
@@ -57,21 +94,3 @@ void delay(unsigned int tiempo)
 	  for(j=0;j<1275;j++);
 }
 
-void dis (unsigned int num)
-{
-	if (num == 1){
-		GPIO = digit1[1];
-	}
-	else {
-		GPIO = digit2[3];
-	}
-}
-
-void calculoNumero(unsigned int clock, unsigned int* num1, unsigned int* num2){
-	static unsigned int value = 0;
-    if (clock != 0) {
-        value = clock;
-    }
-    value = (value * 1664525UL + 1013904223UL) % 4294967296UL;
-    return;
-}
