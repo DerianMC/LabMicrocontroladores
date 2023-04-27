9fc44f6539a628dfd306712dc9d514cc7238f590
DerianMC <derian.monge@ucr.ac.cr>
2023-04-26 23:57:02 -0600

    Circuito mayormente listo. Falta el switch para activar transmisión serial. Medición DC correcta. Prototipo para AC casi listo. Se tiene el error de que se mantiene el valor máximo y no cambia si se reduce la amplitud de la onda. Falta reparar este error.

--- Lab03/Lab03.ino
+++ Lab03/Lab03.ino
@@ -3,7 +3,22 @@
 // Create an instance of the PCD8544 library
 Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 
-int val = 0;
+float V1 = 0;
+float V2 = 0;
+float V3 = 0;
+float V4 = 0;
+
+float V1_prev = 0;
+float V2_prev = 0;
+float V3_prev = 0;
+float V4_prev = 0;
+
+int modo = 8;
+
+
+
+
+
 
 void setup() {
   // Initialize the display
@@ -14,27 +29,59 @@ void setup() {
   display.clearDisplay();
   // Set the cursor to the top-left corner
   display.setCursor(0,0);
-  // Print "Hello World!"
-  display.println("V1:");
-  display.println("V2");
-  display.println("V3");
-  display.println("V4");
-
-  // Display the contents of the buffer
-  display.display();
 }
 
 void loop() {
-
-  int val = analogRead(A0);
-
   display.clearDisplay();
-  display.setCursor(0,10);
-  display.print("Var value ");
-  display.print(val);
-  display.setCursor(0,20);
-  display.print("Var value 2 ");
-  display.print(val);
-  display.display();
+
+  if(digitalRead(modo) ==HIGH){
+    display.setCursor(70,35);
+    display.print("DC:");
+    V1 = ((analogRead(A0)-510)*(-0.04902));
+    V2 = ((analogRead(A1)-510)*(-0.04902));
+    V3 = ((analogRead(A2)-510)*(-0.04902));
+    V4 = ((analogRead(A3)-510)*(-0.04902));
+  }
+  else{
+    display.setCursor(70,35);
+    display.print("AC:");
+    if(V1_prev < ((analogRead(A0)-510)*(-0.04902))){
+      V1_prev = ((analogRead(A0)-510)*(-0.04902));
+    }
+    if(V2_prev < ((analogRead(A1)-510)*(-0.04902))){
+      V2_prev = ((analogRead(A1)-510)*(-0.04902));
+    }
+    if(V3_prev < ((analogRead(A2)-510)*(-0.04902))){
+      V3_prev = ((analogRead(A2)-510)*(-0.04902));
+    }
+    if(V4_prev < ((analogRead(A3)-510)*(-0.04902))){
+      V4_prev = ((analogRead(A3)-510)*(-0.04902));
+    }
+    V1 = (0.707)*V1_prev;
+    V2 = (0.707)*V2_prev;
+    V3 = (0.707)*V3_prev;
+    V4 = (0.707)*V4_prev;
+  }
+
+  
+
+  
+  display.setCursor(10,5);
+  display.print("V1:");
+  display.print(V1);
+
+  display.setCursor(10,15);
+  display.print("V2:");
+  display.print(V2);
+
+  display.setCursor(10,25);
+  display.print("V3:");
+  display.print(V3);
+
+  display.setCursor(10,35);
+  display.print("V4:");
+  display.print(V4);
+
+  display.display(); 
   // Do nothing
 }
