6780ea5abd4aaa6f076a2165953dcb61fdcfe20b
DerianMC <derian.monge@ucr.ac.cr>
2023-05-24 12:03:07 -0600

    Se ininia nuevamente. Esta bez utilizando la comunicación serial con la pantalla

---
+++ Lab04/clock.d
@@ -0,0 +1,24 @@
+clock.o: clock.c \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.0/include/stdbool.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/pwr.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/pwr.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/cm3/nvic.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/dispatch/nvic.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/nvic.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/cm3/systick.h \
+ clock.h
---
+++ Lab04/sdram.d
@@ -0,0 +1,27 @@
+sdram.o: sdram.c /usr/lib/gcc/arm-none-eabi/12.2.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.0/include/stdbool.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/gpio.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/pwr.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/pwr.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/fsmc.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/fmc.h \
+ /home/derianmc/Desktop/STM32_examples/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/fmc_common_f47.h \
+ clock.h sdram.h
