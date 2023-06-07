e1dba4252441665a6f2da97f22ab8dab917f2b89
DerianMC <derian.monge@ucr.ac.cr>
2023-06-06 21:44:19 -0600

    Se cambia la plataforma IoT a utilizar. Se pasa de ThingSpeak a Blynk.

--- Proyecto/Proyecto.ino
+++ Proyecto/Proyecto.ino
@@ -1,39 +1,82 @@
-#include "ThingSpeak.h"
-#include "WiFi.h"
+/*************************************************************
+
+  This is a simple demo of sending and receiving some data.
+  Be sure to check out other examples!
+ *************************************************************/
+
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID           "TMPL2D65xrkHX"
+#define BLYNK_TEMPLATE_NAME         "Quickstart Template"
+#define BLYNK_AUTH_TOKEN            "jOWGzQaYytDom3RsnsBKFzXxn1KRiX6U"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
 
 #define LED_PIN 25
 
-const char* ssid = "Redmi_note";
-const char* password = "ahkshrnnh";
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 
-unsigned long channelID = 2154048;                //ID de vuestro canal.
-const char* WriteAPIKey = "1L1RJPVINEXT4B47";     //Write API Key de vuestro canal.
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Redmi_note";
+char pass[] = "ahkshrnnh";
 
-WiFiClient cliente;
+BlynkTimer timer;
 
-int prueba = 0;
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V0)
+{
+  // Set incoming value from pin V0 to a variable
+  int value = param.asInt();
+  int valor = 0;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
-  WiFi.begin(ssid, password);
-  WiFi.begin(ssid,password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  if(param.asInt() == 1){
+    digitalWrite(LED_PIN,HIGH);
+    delay(3000);
+    
   }
-  Serial.println("Wifi conectado!");
-  Serial.println("-----------------------");
-
-  ThingSpeak.begin(cliente);
+  else{
+    digitalWrite(LED_PIN,LOW);
+  }
+  Blynk.virtualWrite(V0, valor);
 }
-void loop() {
-  digitalWrite(LED_PIN,HIGH);
-  delay(16);
-  ThingSpeak.setField(1, prueba);
-  digitalWrite(LED_PIN,LOW);
-  delay(16);
-  prueba = prueba+1;
-  ThingSpeak.writeFields(channelID,WriteAPIKey);
-  Serial.println("Datos enviados a ThingSpeak!");
+
+// This function is called every time the device is connected to the Blynk.Cloud
+BLYNK_CONNECTED()
+{
+  // Change Web Link Button message to "Congratulations!"
+  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
+
+// This function sends Arduino's uptime every second to Virtual Pin 2.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V2, millis() / 1000);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  pinMode(25, OUTPUT);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
 }
